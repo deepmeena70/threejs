1166ca86a70cc06b40e7202e4d1ebc20f69feda8
Deep <45520903+deepmeena70@users.noreply.github.com>
2023-02-05 02:05:58 +0530

    textures added

---
+++ Image Box/main.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import WebGL from 'capabilities';
+
+(() => {
+  console.log('javascript is running...');
+  const scene = new THREE.Scene();
+
+  scene.background = new THREE.Color('#1E1E24');
+
+  const camera = new THREE.PerspectiveCamera(
+    300,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10000
+  );
+
+  const loader = new THREE.CubeTextureLoader();
+
+  loader.setPath( 'textures/' );
+
+  const textureCube = loader.load( [
+    'fox.png', 'lion.png',
+    'blackforest.png', 'zebra.jpg',
+    'moth.png', 'boat.jpg'
+  ] );
+
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  camera.position.set(1000, 1000, 1000);
+
+  const geometry = new THREE.BoxGeometry(1000, 1000, 1000);
+  const material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
+
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  controls.update();
+
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x -= 0.002;
+    mesh.rotation.y -= 0.005;
+    mesh.rotation.z -= 0.003;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+  };
+
+  //   check WebGL support by browsers
+  if (WebGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    animate();
+  } else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+  }
+})();
